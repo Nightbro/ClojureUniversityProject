0d4a8dfe099f3bea85c977f3215de62dd9ed8550
Nightbro <jordanovicivan@outlook.com>
2023-05-22 17:11:33 +0200

    Configured the project to be able to do interactive development

--- my-body-app/project.clj
+++ my-body-app/project.clj
@@ -5,9 +5,18 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [compojure "1.7.0"]
-                 [ring/ring-core "1.9.2"]
+                 [ring/ring-core "1.9.6"]
+                 [ring/ring "1.9.6"]
+                 [ring/ring-mock "0.4.0"]
                  [ring/ring-defaults "0.3.4"]
                  [ring/ring-jetty-adapter "1.9.6"]
+                 [lein-ring "0.12.5"]
                  [hiccup "1.0.5"]]
-  :main my-body-app.core 
-  :repl-options {:init-ns my-body-app.core})
+  :main my-body-app.core
+  :ring {:handler my-body-app.core/app
+         :init    my-body-app.core/init
+         :destroy my-body-app.core/destroy}
+  :repl-options {:init-ns my-body-app.core}
+  :profiles
+  {:dev {:dependencies [[ring/ring-mock "0.4.0"]]
+         :plugins [[lein-ring "0.12.5"]]}})
